6918c120559519e1113743e3aa6c62d7297dc5c1
mohan <mohan.dash@buawei.com>
2025-04-23 13:06:59 +0000

    implement fine-tuning script for Llama model with LoRA integration

---
+++ finetune_llama_from_adapter_checkpoint.py
@@ -0,0 +1,211 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig, get_scheduler
+from bitsandbytes.optim import Adam8bit,PagedAdam32bit
+from peft import LoraConfig, get_peft_model, PeftModel, PeftConfig
+from peft import prepare_model_for_kbit_training
+import torch
+from IPython.display import  clear_output
+import time
+import gc
+import os
+from torch.utils.data import Dataset, DataLoader
+from datasets import load_dataset
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+DEFAULT_MODEL = "meta-llama/Llama-3.2-3B-Instruct"#"meta-llama/Llama-3.2-3B-Instruct","/home/nas/buffer/mohan.dash/llama_3_2_3B"
+
+bnb_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_quant_type='nf4',
+        bnb_4bit_compute_dtype=torch.bfloat16,
+        bnb_4bit_use_double_quant=False,
+    )
+
+
+model = AutoModelForCausalLM.from_pretrained(
+    DEFAULT_MODEL,
+    quantization_config=bnb_config,
+    use_safetensors=True,
+    device_map=device,
+)
+
+print(model.get_memory_footprint()/(1024*1024)) 
+
+tokenizer = AutoTokenizer.from_pretrained(DEFAULT_MODEL, use_safetensors=True)
+tokenizer.pad_token_id = tokenizer.eos_token_id
+
+def flush():
+    torch.cuda.empty_cache()
+    gc.collect()
+    torch.cuda.empty_cache()
+    gc.collect()
+    
+    
+dataset = load_dataset('OdiaGenAI/odia_domain_context_train_v1')
+
+class LlamaDataset(Dataset):
+    def __init__(self, dataset):
+        self.data = dataset
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        sample = self.data[idx]
+        question=sample['instruction']
+        answer = sample['output']
+        prompt = f'''<|begin_of_text|> <|start_header_id|>user<|end_header_id|>\n\n{question}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n'''
+        full_text = prompt+f'''{answer}।<|eot_id|>'''
+
+        tokenized = tokenizer(full_text, truncation=True, add_special_tokens=False, padding="max_length", max_length=1000)
+
+        input_ids = tokenized["input_ids"]
+        attention_mask = tokenized["attention_mask"]
+
+        # Tokenize just the prompt to get the split point
+        prompt_ids = tokenizer(prompt, add_special_tokens=False)["input_ids"]
+        answer_start = len(prompt_ids)
+
+        # Mask everything before answer_start
+        labels = [-100] * answer_start + input_ids[answer_start:]
+        # Mask out padding as well
+        labels = [
+            label if token != tokenizer.pad_token_id else -100
+            for label, token in zip(labels, input_ids)
+        ]
+    
+        return {
+        "input_ids": torch.tensor(input_ids),
+        "attention_mask": torch.tensor(attention_mask),
+        "labels": torch.tensor(labels)
+    }
+        
+train_dataset = LlamaDataset(dataset['train'])
+train_dataloader = DataLoader(train_dataset, batch_size=2, shuffle=True)
+
+model = prepare_model_for_kbit_training(model)
+model = PeftModel.from_pretrained(model, 'adapter', is_trainable=True) # Biggest chnage in this script
+
+
+def generate_eval(model,idx=5,disable_lora=False):
+    
+    model.config.use_cache = True
+    sample=dataset['train'][idx]
+    question=sample['instruction']
+    answer = sample['output']
+    chat_template = f'''<|begin_of_text|> <|start_header_id|>user<|end_header_id|>\n\n{question}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n'''
+    inputs = tokenizer(chat_template , return_tensors="pt").to(device)
+    # print(prompt)
+
+    model.eval()
+
+
+    if disable_lora:
+        with model.disable_adapter():
+            output = model.generate(
+                **inputs,
+                do_sample=False,
+                max_new_tokens=300,
+                repetition_penalty=1.3,
+                temperature=0.7,         # Optional: smooth randomness
+                top_k=50,                # Optional: top-k sampling
+                top_p=0.9                # Optional: nucleus sampling
+            )
+    else:
+        output = model.generate(
+        **inputs,
+        do_sample=False,
+        max_new_tokens=512,
+        repetition_penalty=1.3,
+        temperature=0.7,         # Optional: smooth randomness
+        top_k=50,                # Optional: top-k sampling
+        top_p=0.9                # Optional: nucleus sampling
+        )
+
+    processed_text = tokenizer.decode(output[0], skip_special_tokens=False)
+    
+    model.train()
+
+    return processed_text
+#########################################################################################################################
+#########################################################################################################################
+#########################################################################################################################
+#########################################################################################################################
+model.config.use_cache = False
+model.config.pretraining_tp = 1
+gradient_accumulation_steps = 8
+max_steps=5000
+max_loss = 1e9
+global_step= 0
+
+
+# Define optimizer
+optimizer = PagedAdam32bit(model.parameters(), lr=2e-4)
+lr_scheduler = get_scheduler(
+    name="linear",
+    optimizer=optimizer,
+    num_warmup_steps=0,
+    num_training_steps=max_steps,
+)
+
+# Path to your saved checkpoint
+save_path = "/home/nas/buffer/mohan.dash/llama_3_finetuned/model_checkpoint.pt"
+# Load checkpoint
+checkpoint = torch.load(save_path, map_location='cuda' if torch.cuda.is_available() else 'cpu') # Biggest chnage in this script
+# Restore model, optimizer, scheduler, and step
+optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+lr_scheduler.load_state_dict(checkpoint['lr_scheduler_state_dict'])
+global_step = checkpoint['global_step']
+print('>'*30,f"Checkpoint loaded from {save_path} at step {global_step}")
+
+
+# Training loop
+model.train()
+
+while global_step< max_steps:
+    for step,batch in enumerate(train_dataloader):
+        model.config.use_cache = False
+        model.train()
+        
+        # Forward pass
+        outputs = model(input_ids=batch['input_ids'].to('cuda'), attention_mask=batch['attention_mask'].to('cuda'), labels=batch['labels'].to('cuda'))
+        loss = outputs.loss
+        loss = loss / gradient_accumulation_steps  # Normalize loss
+        loss.backward()
+        
+        if (step + 1) % gradient_accumulation_steps == 0:
+            optimizer.step()
+            lr_scheduler.step()
+            optimizer.zero_grad()
+        
+        global_step += 1
+        if global_step >= max_steps:
+            break
+        
+        if global_step % 20 == 0:
+            pred = generate_eval(model=model,idx=50,disable_lora=False)
+            
+            # Save predictions to a text file named with current step
+            pred_filename = os.path.join('/home/nas/buffer/mohan.dash/llama_3_finetuned', f"{global_step}.txt")
+            with open(pred_filename, "w") as f:
+                f.write(pred if isinstance(pred, str) else str(pred))
+            
+            print('*'*20,step+1,'*'*20)
+            print("Predictions:", pred)
+            print('*'*20,'end','*'*20)
+            
+        # if loss.item() < max_loss:
+        #     model.save_pretrained('/home/nas/buffer/mohan.dash/llama_3_finetuned/adapter')
+        #     max_loss = loss.item()
+        #     save_path = "/home/nas/buffer/mohan.dash/llama_3_finetuned/model_checkpoint.pt"
+
+        #     torch.save({
+        #         'optimizer_state_dict': optimizer.state_dict(),
+        #         'lr_scheduler_state_dict': lr_scheduler.state_dict(),
+        #         'global_step': global_step
+        #     }, save_path)
+            
+            
+            
+        
+        print(f"Epoch {global_step + 1}/{max_steps}, Loss: {loss.item():.4f}")
